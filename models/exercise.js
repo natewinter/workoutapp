7f20c56b6805270a82401e9be3a56f6af2806048
Josh Brown <joshbrown2794@gmail.com>
2020-10-29 18:58:04 -0700

    schema and server start

---
+++ models/exercise.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema ({
+name:{
+    type:String,
+    trim:true,
+    required:"Exercise name required!"
+},
+type:{
+    type:String,
+    trim:true,
+    required:"Type of exercise required!"
+},
+weight:{
+    type:Number  
+},
+sets:{
+    type:Number,
+    required:"Number of sets required!"
+
+},
+reps:{
+    type:Number,
+    required:"Number of reps required!"
+},
+duration:{
+    type:Number,
+    required:"Duration of exercise required!"
+},
+distance:{
+    type:Number,
+}
+})
