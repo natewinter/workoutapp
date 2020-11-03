661a73fe64125b6ad92ee74afcbca8b9b62da4a0
Josh Brown <joshbrown2794@gmail.com>
2020-11-02 16:46:50 -0800

    updating createworkouts function

--- public/index.js
+++ public/index.js
@@ -1,41 +1,61 @@
 // const { on } = require("../models/exercise");
 
-function createWorkouts(){
+function createWorkouts() {
     //  $(".workoutBtns").empty();
-     $.ajax({
-    url:"/workoutplan",
-    method:"GET"
-}).then(allWorkouts => {
-    console.log(allWorkouts)
-    //const theWOrkoutIwant = allWorkouts.find(w => w.id === id)
-    // allWorkouts.map( (workout) =>
-    for( var i = 0; i <allWorkouts.length;i++){
-        $(".workoutBtns").append(
-            `<button class="btn workoutBtn" value=${allWorkouts[i]._id}>${allWorkouts[i].name} </button>`
-        )
-    };
-    $(".workoutBtn").click(function(event){
-        event.preventDefault();
-        $.ajax({
-            url:`/populated/`+`${this.value}`,
-            method:"GET"
-        }).then(
-            console.log(this.value)
-        )
+    $.ajax({
+        url: "/workoutplan",
+        method: "GET"
+    }).then(allWorkouts => {
+        console.log(allWorkouts)
+        //const theWOrkoutIwant = allWorkouts.find(w => w.id === id)
+        // allWorkouts.map( (workout) =>
+        for (var i = 0; i < allWorkouts.length; i++) {
+            $(".workoutBtns").append(
+                `<button class="btn workoutBtn" value=${allWorkouts[i]._id}>${allWorkouts[i].name} </button>`
+            )
+        };
+        $(".workoutBtn").click(function (event) {
+            event.preventDefault();
+            $.ajax({
+                url: `/populated/` + `${this.value}`,
+                method: "GET"
+            }).then(Workout =>{
+                for (var i=0;i<Workout.exercises.length;i++) {
+                    if (Workout.exercises[i].type === "cardio") {
+                        $(".cardio-section").append(`
+                        <div class="exercise-entry" style="padding:1rem">
+                        <p style="margin:0">Name: ${Workout.exercises[i].name}</p>
+                        <p style="margin:0">Duration: ${Workout.exercises[i].duration}</p>
+                        <p style="margin:0">Distance: ${Workout.exercises[i].distance}</p>
+                        </div>
+                    `)
+                    }
+                    else if (Workout.exercises[i].type === "weights") {
+                        $(".weights-section").append(`
+                        <div class="exercise-entry" style="padding:1rem">
+                        <p style="margin:0">Name: ${Workout.exercises[i].name}</p>
+                        <p style="margin:0">Weights: ${Workout.exercises[i].weight}</p>
+                        <p style="margin:0">Reps: ${Workout.exercises[i].reps}</p>
+                        <p style="margin:0">Sets: ${Workout.exercises[i].sets}</p>
+                        </div>
+                    `)
+                    }
+                }
+            })
+        })
     })
-})
 }
 
 
-$("#submitCardio").click(function(event){
+$("#submitCardio").click(function (event) {
     event.preventDefault();
     const cardio = {
-        name:$("#name").val().trim(),
-        duration:$("#duration").val().trim(),
-        distance:$("#distance").val().trim()
+        name: $("#name").val().trim(),
+        duration: $("#duration").val().trim(),
+        distance: $("#distance").val().trim()
     };
     $.ajax({
-        url:`/submit/`
+        url: `/submit/`
     })
 })
 // function editWorkout(id){
@@ -46,7 +66,7 @@ $("#submitCardio").click(function(event){
 //         url:"/workoutplan/"+id,
 //         method: "GET"
 //     }).then(workout => {
-      
+
 //         })
 
 //     })
@@ -54,19 +74,20 @@ $("#submitCardio").click(function(event){
 function myFunction() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  } 
+}
+
 function myFunction2() {
     var x = document.getElementById("myDIV2");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  } 
+}
 
 
 $(document).ready(() => {
